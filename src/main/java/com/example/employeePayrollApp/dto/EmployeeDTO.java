1404be82758c4426c3f2d6aa5653fe315b57b41a
Abhishek <ak1824871@gmail.com>
2025-03-07 16:37:01 +0530

    Added: Lombok Library

--- src/main/java/com/example/employeePayrollApp/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeePayrollApp/dto/EmployeeDTO.java
@@ -2,36 +2,15 @@ package com.example.employeePayrollApp.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class EmployeeDTO {
+
+public @Data class EmployeeDTO {
     private String department;
     private String name;
     private double salary;
 
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
+    public EmployeeDTO(String department, String name, double salary){
         this.department = department;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
         this.salary = salary;
     }
 }
